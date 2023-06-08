152800dc21ba4e21a748bca6fdd73ec61b391d24
Logan Davis <LMDavis040@gmail.com>
2023-06-08 16:50:18 +0000

    add index.html and css files

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,10 +20,17 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+        var circle
+        var circles
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas,true,true,"#999",2);
+            physikz.addRandomVelocity(circle,canvas);
+            view.addChild(cricle);
+            circles.push(circle);
+    
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
 
